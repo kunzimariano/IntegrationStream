136b238551fa76f7be493f082c2e00a138c9934e
kunzimariano <kunzi.mariano@gmail.com>
2015-05-05 16:26:34 -0300

    Added initial stats projections for digests and inboxes.

---
+++ src/app/projections/inboxCommitsStats.js
@@ -0,0 +1,27 @@
+function increaseObjectProperty(obj, p, state) {
+  if (state[obj].hasOwnProperty(p)) {
+    state[obj][p]++;
+  } else {
+    state[obj][p] = 1;
+  }
+}
+
+fromCategory('inboxCommits')
+  .foreachStream()
+  .when({
+    "$init": function(state, ev) {
+      return {
+        authorCount: {},
+        committerCount: {}
+      }
+    },
+    "$any": function(state, ev) {
+      if (ev.eventType !== "GitHubCommitReceived") return;
+
+      var author = ev.data.commit.author.name;
+      var committer = ev.data.commit.committer.name;
+
+      increaseObjectProperty('authorCount', author, state);
+      increaseObjectProperty('committerCount', committer, state);
+    }
+  });
