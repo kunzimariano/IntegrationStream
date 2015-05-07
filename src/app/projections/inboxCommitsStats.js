ef61edcc47eb5ca7f57575bab36260beb71831ab
kunzimariano <kunzi.mariano@gmail.com>
2015-05-07 17:20:22 -0300

    Modified stats projections so they can count the amount of time a file was changed.

--- src/app/projections/inboxCommitsStats.js
+++ src/app/projections/inboxCommitsStats.js
@@ -6,22 +6,29 @@ function increaseObjectProperty(obj, p, state) {
   }
 }
 
+function modifiedFiles(originalMessage, state) {
+  if (!originalMessage) return;
+
+  originalMessage.modified.forEach(function(fileName) {
+    increaseObjectProperty('fileCount', fileName, state);
+  });
+}
+
 fromCategory('inboxCommits')
   .foreachStream()
   .when({
     "$init": function(state, ev) {
       return {
         authorCount: {},
-        committerCount: {}
+        committerCount: {},
+        fileCount: {}
       }
     },
     "$any": function(state, ev) {
       if (ev.eventType !== "GitHubCommitReceived") return;
 
-      var author = ev.data.commit.author.name;
-      var committer = ev.data.commit.committer.name;
-
-      increaseObjectProperty('authorCount', author, state);
-      increaseObjectProperty('committerCount', committer, state);
+      increaseObjectProperty('authorCount', ev.data.commit.author.name, state);
+      increaseObjectProperty('committerCount', ev.data.commit.committer.name, state);
+      modifiedFiles(ev.data.originalMessage, state);
     }
   });
