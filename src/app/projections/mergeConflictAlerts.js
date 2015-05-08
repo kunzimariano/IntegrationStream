0b52546d6efdcfad63ae73bb8a2bf0c18b602144
MatiasHeffel <matiasheffel@hotmail.com>
2015-05-07 21:02:08 -0300

    Added projection

---
+++ src/app/projections/mergeConflictAlerts.js
@@ -0,0 +1,52 @@
+var addFilesModified = function(pushDate, files, branch, state) {
+  if (state.hasOwnProperty(pushDate)) {
+    files.forEach(function(file) {
+      if (state[pushDate][file]) {
+        if (!state[pushDate][file].hasOwnProperty(branch)) {
+          state[pushDate][file][branch] = 1;
+          emit("mergeConflictAlert", "conflictDetected", {
+            'file': file,
+            'branches': state[pushDate][file]
+          })
+        } else {
+          if (Object.keys(state[pushDate][file]).length > 1) {
+          	state[pushDate][file][branch]++;
+            emit("mergeConflictAlert", "conflictknown", {
+              'file': file,
+              'branches': state[pushDate][file]
+            });
+          }
+        }
+      } else {
+        state[pushDate][file][branch] = 1;
+      }
+    });
+  } else {
+    //Removes the entry from the previus pushDate
+    delete state[Object.keys(state)[0]];
+    state[pushDate] = {};
+    files.forEach(function(file) {
+      state[pushDate][file] = {};
+      state[pushDate][file][branch] = 1;
+    });
+  }
+}
+
+fromCategory('inboxCommits')
+  .foreachStream()
+  .when({
+    "$init": function(state, ev) {
+      return {};
+    },
+    "$any": function(state, ev) {
+      if (!ev.data.pushDate) return;
+
+      var pushDate = ev.data.pushDate.split('T')[0];
+      var added = ev.data.originalMessage.added;
+      var modified = ev.data.originalMessage.modified;
+      var removed = ev.data.originalMessage.removed;
+      var files = added.concat(modified).concat(removed);
+      var branch = ev.data.branch;
+      addFilesModified(pushDate, files, branch, state);
+    }
+  });
